a2ff7982c95200078c6211c42a9e50e9c0324e21
felixo2022 <walterbarax@gmail.com>
2022-08-25 02:19:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    return callback()
+
+
+}
+function returnsANamedFunction() {
+    return function names(){}
+
+    
+}
+function returnsAnAnonymousFunction() {
+    return function(){}
+
+
+}
